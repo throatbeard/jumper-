4a92846a231125c69d486ccf4bba6d1e6508450c
nightflight21 <104380966+nightflight21@users.noreply.github.com>
2022-05-23 10:03:40 -0700

    structure of word file

---
+++ Word.cs
@@ -0,0 +1,15 @@
+namespace Jumper{
+    class Word{
+        public List<string> wordSelection = new List<string>();
+        public string currentWord;
+
+        public string chooseWord() {
+            return "word";
+        }
+
+        public int compareGuess() {
+            //returns index of where letter matches
+            return 0;
+        }
+    }
+}
