d6ad092acf1cd4f1a4353a6eb08183c49eb5a0de
xter7856 <markc7856@yahoo.com>
2022-05-25 10:31:59 -0600

    commands on Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-﻿namespace jumper
+﻿namespace jumper                                    //name of the program
 {
-    class Program
+    class Program                                   //the main class, the program class
     {
-        static void Main(string[] args)
+        static void Main(string[] args)             //main
         {
-            Director director = new Director();
-            director.StartGame();
+            Director director = new Director();     //the class that does it all, the director class
+            director.StartGame();                   //startgame is the funcitont hat runs other functions
         }
     }
 }
